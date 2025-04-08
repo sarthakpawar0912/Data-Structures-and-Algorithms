b5a635afc97a2c1035b5145eb559fd79ad67c7f7
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-09 00:14:47 +0530

    Updated files with recent changes

--- Array/src/Sorting/Cycle/CycleSort.java
+++ Array/src/Sorting/Cycle/CycleSort.java
@@ -2,12 +2,17 @@ package Sorting.Cycle;
 
 import java.util.Arrays;
 public class CycleSort {
+
 	public static void main(String[] args) {
+
 		int[] arr= {5,4,3,2,1};
 		cycle(arr);
 		System.out.println(Arrays.toString(arr));
+
 	}
+
 	static void cycle(int arr[]) {
+
 		int i=0;
 		while(i<arr.length) {
 			int correct=arr[i]-1;
@@ -18,9 +23,12 @@ public class CycleSort {
 			}
 		}
 	}
+
 	private static void swap(int[] arr, int i, int correct) {
+
         int temp = arr[i];
         arr[i] = arr[correct];
         arr[correct] = temp;
-    }	
+
+	}
 }
