d8d577e1b9c4ef6f4a2b8543430340fefab390ce
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-09 00:16:52 +0530

    Updated files with recent changes

--- Recursion/src/com/basicExamples/BinarySearchWithRecursion.java
+++ Recursion/src/com/basicExamples/BinarySearchWithRecursion.java
@@ -3,11 +3,15 @@ package com.basicExamples;
 public class BinarySearchWithRecursion {
 
 	public static void main(String[] args) {
+
 		int [] arr= {1,2,3,4,55,66,78};
 		int target=78;
 		System.out.println(search(arr,target,0,arr.length-1));
+
 	}
+
 	static int search(int[] arr,int target,int s,int e) {
+
 		if(s>e) {
 			return -1;
 		}
