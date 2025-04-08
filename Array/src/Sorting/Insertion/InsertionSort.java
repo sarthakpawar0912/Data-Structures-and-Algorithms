e190a622510ae0d3bfeef0e68e6fa0e19c0bd711
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-09 00:14:08 +0530

    Updated files with recent changes

--- Array/src/Sorting/Insertion/InsertionSort.java
+++ Array/src/Sorting/Insertion/InsertionSort.java
@@ -2,6 +2,7 @@ package Sorting.Insertion;
 import java.util.Arrays;
 
 public class InsertionSort {
+
     public static void main(String[] args) {
         int[] arr = {4,5,1,2,3};
         insertion(arr);
@@ -25,4 +26,5 @@ public class InsertionSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
 }
