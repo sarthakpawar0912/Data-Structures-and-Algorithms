38dd9a0e9fd686263e6a318cac08d3b6c03539b9
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-09 00:17:03 +0530

    Updated files with recent changes

--- Recursion/src/com/basicExamples/DigitProd.java
+++ Recursion/src/com/basicExamples/DigitProd.java
@@ -6,18 +6,10 @@ public class DigitProd {
 		int ans=sum(1342);
 		System.out.println(ans);
 	}
-	
-	
 	static int sum(int n) {
 		if(n ==0) {
 			return 0;
 		}
-		
-		
 		return (n%10) +sum(n/10);		
 	}
-	
-	
-	
-	
 }
