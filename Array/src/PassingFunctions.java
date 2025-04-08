83d01bdad90629ed65a61be41c4a70e0a744fd7a
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-09 00:12:58 +0530

    Updated files with recent changes

--- Array/src/PassingFunctions.java
+++ Array/src/PassingFunctions.java
@@ -7,8 +7,6 @@ public class PassingFunctions {
 		System.out.println(Arrays.toString(nums));
 		change(nums);
 		System.out.println(Arrays.toString(nums));
-
-		
 	}
 	static void change(int[]arr) {
 		arr[0]=99;
